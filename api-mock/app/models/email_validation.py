cddb7b82481f4dc11777e8377f40638513739e0b
ninababic <nina.babic@productdock.com>
2024-10-19 22:38:52 +0200

    Refactor; Modify Pydantic config

--- api-mock/app/models/email_validation.py
+++ api-mock/app/models/email_validation.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 from pydantic.alias_generators import to_camel
 
 
 class EmailValidationBaseModel(BaseModel):
-    model_config = ConfigDict(alias_generator=to_camel)
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
 
 class EmailValidationRequest(EmailValidationBaseModel):
--- keycloak/src/main/java/com.productdock.keycloak.extension/CustomRegistrationUserCreation.java
+++ keycloak/src/main/java/com.productdock.keycloak.extension/CustomRegistrationUserCreation.java
@@ -21,10 +21,8 @@ import org.keycloak.authentication.forms.RegistrationUserCreation;
 import org.keycloak.events.Details;
 import org.keycloak.events.Errors;
 import org.keycloak.forms.login.LoginFormsProvider;
-import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.FormMessage;
 import org.keycloak.services.messages.Messages;
-import org.keycloak.services.validation.Validation;
 import org.keycloak.userprofile.Attributes;
 import org.keycloak.userprofile.UserProfile;
 import org.keycloak.userprofile.ValidationException;
@@ -32,13 +30,16 @@ import org.keycloak.userprofile.ValidationException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.productdock.keycloak.extension.CustomMessages.EMAIL_DOMAIN_NOT_ALLOWED;
 import static com.productdock.keycloak.extension.CustomMessages.NETWORK_ISSUES;
 import static java.lang.String.format;
 import static java.lang.System.getenv;
+import static org.keycloak.models.UserModel.EMAIL;
+import static org.keycloak.models.UserModel.USERNAME;
+import static org.keycloak.services.validation.Validation.getFormErrorsFromValidation;
 
 @JBossLog
 @AutoService(FormActionFactory.class)
@@ -62,18 +63,18 @@ public class CustomRegistrationUserCreation extends RegistrationUserCreation {
     @Override
     public void validate(ValidationContext context) {
         log.info("Validating user data");
-        List<FormMessage> errors = new java.util.ArrayList<>(Collections.emptyList());
+        List<FormMessage> errors = new ArrayList<>();
+
         MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
         context.getEvent().detail(Details.REGISTER_METHOD, "form");
 
         UserProfile profile = getOrCreateUserProfile(context, formData);
-
         Attributes attributes = profile.getAttributes();
 
-        String email = attributes.getFirst(UserModel.EMAIL);
-        String username = attributes.getFirst(UserModel.USERNAME);
-        context.getEvent().detail(Details.EMAIL, email);
+        String email = attributes.getFirst(EMAIL);
+        String username = attributes.getFirst(USERNAME);
 
+        context.getEvent().detail(Details.EMAIL, email);
         context.getEvent().detail(Details.USERNAME, username);
 
         if (context.getRealm().isRegistrationEmailAsUsername()) {
@@ -83,10 +84,10 @@ public class CustomRegistrationUserCreation extends RegistrationUserCreation {
         try {
             profile.validate();
         } catch (ValidationException pve) {
-            errors.addAll(Validation.getFormErrorsFromValidation(pve.getErrors()));
+            errors.addAll(getFormErrorsFromValidation(pve.getErrors()));
 
             if (pve.hasError(Messages.EMAIL_EXISTS, Messages.INVALID_EMAIL)) {
-                context.getEvent().detail(Details.EMAIL, attributes.getFirst(UserModel.EMAIL));
+                context.getEvent().detail(Details.EMAIL, attributes.getFirst(EMAIL));
             }
 
             if (pve.hasError(Messages.EMAIL_EXISTS)) {
@@ -102,9 +103,7 @@ public class CustomRegistrationUserCreation extends RegistrationUserCreation {
         }
 
         try {
-            boolean emailDomainAllowed = emailDomainAllowed(email);
-
-            if (!emailDomainAllowed) {
+            if (!emailDomainAllowed(email)) {
                 log.info("Provided email domain is not allowed");
                 context.error(EMAIL_DOMAIN_NOT_ALLOWED);
                 errors.add(new FormMessage(null, EMAIL_DOMAIN_NOT_ALLOWED));
@@ -145,7 +144,7 @@ public class CustomRegistrationUserCreation extends RegistrationUserCreation {
                 HttpEntity entity = response.getEntity();
 
                 if (entity == null) {
-                    return false;
+                    throw new IOException(format("Invalid response from Mock API: %s", statusCode));
                 }
 
                 return domainAllowed(entity);
